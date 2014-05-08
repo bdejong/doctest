27c7a6ab1dd6fb2b402a17bac8dabc0659b7bf4f
Bram de Jong <bram.dejong@native-instruments.de>
2014-05-08 13:49:10 +0200

    added ghraphviz...

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx.ext.graphviz'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
